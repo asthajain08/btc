2ea245a9c66d8a4da03894093f3ca7a0a5237137
asthajain497@gmail.com <asthajain497@gmail.com>
2023-07-29 15:28:16 +0530

    complete

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const path = require("path");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1/form', { useNewUrlParser: true, useUnifiedTopology: true });
 var db=mongoose.connection;
-db.on('error', console.log.bind(console, "connection error"));
+db.on('error', console.log.bind(console, "connection error")); 
 db.once('open', function(callback){
    console.log("connection succeeded");
 })
